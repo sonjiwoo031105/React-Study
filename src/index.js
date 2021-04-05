37f70ab70b761d4d12598074dcfdb2626c8b8b6c
Jiwoo Son <jiji432@naver.com>
2021-04-05 09:54:36 +0900

    added lifecycle-methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //import './component-basic/function-component'
 //import './component-intermediate/state-study'
-import './component-intermediate/counter-demo'
+//import './component-intermediate/counter-demo'
+import './component-intermediate/lifecycle-methods'
 
 
 
